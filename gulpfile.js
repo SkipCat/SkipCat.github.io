b559065ee3e9720cd58a02ecad38c1c6d6bd0cc3
SkipCat <cecilehuguet@laposte.net>
2019-05-03 00:19:12 +0200

    Improved style

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function () {
-  return gulp.src('./scss/main.scss')
-      .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-      .pipe(gulp.dest('./css'));
+  return gulp.src('./assets/scss/main.scss')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(gulp.dest('./assets'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('scss/main.scss', gulp.series('styles'));
+  gulp.watch('./assets/scss/main.scss', gulp.series('styles'));
 });
